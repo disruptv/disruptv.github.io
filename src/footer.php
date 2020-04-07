d454d42ab9ec19eae7477c60fccd45c00353637d
Aaron Salley <aaron@disruptv.io>
2020-04-07 13:22:38 -0400

    Ready for Elementor

---
+++ src/footer.php
@@ -0,0 +1,3 @@
+  <?php elementor_theme_do_location( 'footer' ); ?>
+  <?php wp_footer(); ?>
+</body>
---
+++ src/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php elementor_theme_do_location( 'header' ); ?>
+	<?php wp_body_open(); ?>
